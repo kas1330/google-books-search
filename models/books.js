f45d42d914692899ace3e2848eb950fac6eef85e
Kerri Sexton <k.sexton804@gmail.com>
2021-02-21 19:09:44 -0500

    book model done

--- models/books.js
+++ models/books.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    title: { type: String, required: true },
-    author: { type: String, required: true },
-    synopsis: String,
-    date: { type: Date, default: Date.now }
-  });
-  
-  const Book = mongoose.model("Book", bookSchema);
-  
-  module.exports = Book;
+  title: { type: String, required: true },
+  authors: { type: Array, required: true },
+  description: String,
+  image: String,
+  link: String,
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
